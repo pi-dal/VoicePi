080e9c66e29a688b3a1e7ead5d0e042f6a751006
pi-dal <hi@pi-dal.com>
2026-04-08 13:53:44 +0800

    chore: prepare 1.5.0 release

--- Casks/voicepi.rb
+++ Casks/voicepi.rb
@@ -1,8 +1,8 @@
 cask "voicepi" do
-  version "1.4.0"
-  sha256 "ab4daa2fd8b1b26db291f1591364a0219ed422e16b4c8c402175200fd51f55c3"
+  version "1.5.0"
+  sha256 "1fe1890b90d00c643fa9d1b1f935b6cb4036b56fdfdebc916f0bf50000009ff8"
 
-  url "https://github.com/pi-dal/VoicePi/releases/download/v1.4.0/VoicePi-1.4.0.zip"
+  url "https://github.com/pi-dal/VoicePi/releases/download/v1.5.0/VoicePi-1.5.0.zip"
   name "VoicePi"
   desc "macOS menu-bar voice input app built with SwiftPM"
   homepage "https://github.com/pi-dal/VoicePi"
